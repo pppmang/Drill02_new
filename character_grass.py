4732a63cd9dae8e81344265cf3d3b7e236152c87
pppmang <133509483+pppmang@users.noreply.github.com>
2023-09-21 12:01:23 +0900

    원점에서 원 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -12,8 +13,17 @@ delay(1)
 
 def run_circle():
     print('CIRCLE')
+
+    r=200
+    for deg in range(0, 360, 5):
+        x=r*math.cos(math.radians(deg))
+        y=r*math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.1)
+        
     pass
-    
 
 def run_rectangle():
     print('RECTANGLE')
@@ -22,5 +32,6 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
 
 close_canvas()
