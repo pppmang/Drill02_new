d28bc04181cf0eae41e66569226c7ec3df1b9390
pppmang <133509483+pppmang@users.noreply.github.com>
2023-09-21 11:52:36 +0900

    원 운동, 사각 운동의 '무한반복' 확인 완료

--- character_grass.py
+++ character_grass.py
@@ -10,4 +10,17 @@ grass.draw_now(400, 30)
 character.draw_now(400, 90)
 delay(1)
 
+def run_circle():
+    print('CIRCLE')
+    pass
+    
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass    
+
+while True:
+    run_circle()
+    run_rectangle()
+
 close_canvas()
