761a1689c825d0941df6fbe6d0445a13c8de1b47
pppmang <133509483+pppmang@users.noreply.github.com>
2023-09-21 12:20:13 +0900

    그리기 함수 추가

--- character_grass.py
+++ character_grass.py
@@ -6,10 +6,11 @@ open_canvas()
 character=load_image('character.png')
 grass=load_image('grass.png')
 
-clear_canvas_now()
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
-delay(1)
+def render_frame(x, y): # x,y 위치에 캐릭터를 그려줄 수 있는 함수
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.1)
 
 def run_circle():
     print('CIRCLE')
@@ -18,22 +19,16 @@ def run_circle():
     for deg in range(0, 360, 5):
         x=cx+r*math.cos(math.radians(deg))
         y=cy+r*math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.1)
+        render_frame(x, y)
 
 def run_rectangle():
     print('RECTANGLE')
 
     for x in range(50, 750+1, 5):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        delay(0.1)
+        render_frame(x, 90)
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
     break
 
